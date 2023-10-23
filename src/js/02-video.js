878b8e13d84390fecbaab424f2970989954c2663
Serhii1727 <kravchenko087@ukr.net>
2023-10-22 22:49:35 -0400

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
+
+const playerRef = document.querySelector('#vimeo-player')
+
+const player = new Player(playerRef);
+
+const setLocaleStorageData = (event) => {
+    localStorage.setItem('videoplayer-current-time', event.seconds)
+}
+
+const currentSeconds = () => {
+    
+    const currentTime = localStorage.getItem('videoplayer-current-time');
+
+    if (currentTime) {
+        return player.setCurrentTime(currentTime);
+    } 
+
+    return player.setCurrentTime(0)
+}
+
+currentSeconds()
+
+player.on('timeupdate', throttle(setLocaleStorageData, [wait=1000]));
+
+
+
+
+
+
+
+
 
